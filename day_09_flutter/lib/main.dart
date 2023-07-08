08c71679c77fe7ff58ff151bf23c591b7d5484ed
Storm-8 <storm-8@Admin-8s-iMac.local>
2023-07-08 13:06:07 +0800

    add: day 09

---
+++ day_09_flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Title',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Text'),
+          backgroundColor: Colors.green[98],
+        ),
+      )));
+}
