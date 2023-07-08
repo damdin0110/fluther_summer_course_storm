1f9391a46586d48ae9fecc4e12c47cccc2e648ec
Storm-8 <storm-8@Admin-8s-iMac.local>
2023-07-08 12:13:05 +0800

    add:day 09

--- day_09/lib/day_09.dart
+++ day_09/lib/day_09.dart
@@ -1,3 +1,48 @@
-int calculate() {
-  return 6 * 7;
+class Sprite {
+  Sprite(String name) {
+    this.name = name;
+  }
+  String name = 'Sprite';
+  int x = 0;
+  int y = 0;
+  bool isshow = true;
+  int size = 100;
+  int direction = 90;
+  void pointInDeriction(int direction) {
+    this.direction = direction;
+  }
+
+  void setSize(int size) {
+    this.size = size;
+  }
+
+  int getDirection() {
+    return direction;
+  }
+
+  void move(int x) {
+    this.x = x;
+  }
+
+  String say() {
+    return this.name;
+  }
+
+  void changeYby(int y) {
+    this.y = y;
+  }
+}
+
+void main() {
+  final Luca = Sprite('Luca');
+  final Hat = Sprite('Hat');
+  final Kia = Sprite('Kia');
+  print(Luca.say());
+  print(Hat.say());
+  print(Kia.say());
+  print(Luca.getDirection());
+  print(Hat.getDirection());
+  print(Kia.getDirection());
+  Luca.setSize(10);
+
 }
