359fc958be3cf36e669a173615e6ce1a1b9b743b
Storm-8 <storm-8@Admin-8s-iMac.local>
2023-08-22 13:37:41 +0800

    ls23

---
+++ lesson_23_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_23_flutter/layouts/screen_layout.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ScreenLayout(),
+  ));
+}
+
